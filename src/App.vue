b8f3504fc6a978e90b1bc356922b2d5cc337f131
officialjivaro <noreply.jivaro@gmail.com>
2026-06-25 23:47:52 +0900

    Delete src directory

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- Application Root | Renders the complete standalone crossword game -->
-  <CrosswordView />
-</template>
-
-<script setup>
-import CrosswordView from './features/crossword/views/CrosswordView.vue'
-</script>
